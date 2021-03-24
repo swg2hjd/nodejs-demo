baa6f5ebc8929c9ce8700ae1bcfeb180c78c60ff
swg_pro <137707189_pro@qq.com>
2021-03-24 11:47:37 +0800

    My First Commit File app.js!

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+app.get('/',(req,res)=>{
+    res.send('hello nodejs');
+});
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}....`);
+})
